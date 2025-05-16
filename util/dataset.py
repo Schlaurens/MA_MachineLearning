2d0e998a640d0927c5ab44ea6279031d06fa02b4
Laurens Schiefelbein <laurensschiefelbein@gmail.com>
2025-05-17 00:20:06 +0200

    formatting

--- util/dataset.py
+++ util/dataset.py
@@ -1,8 +1,8 @@
 import json
-import os
+from pathlib import Path
 
-import tensorflow as tf
 import numpy as np
+import tensorflow as tf
 
 from . import image as u_image
 
@@ -15,8 +15,9 @@ def get_label_path(directory):
 
     Returns:
         path to the JSON file with the labels
+
     """
-    return os.path.join(directory, "labels.json")
+    return Path(directory) / "labels.json"
 
 
 def get_image_path(directory, name):
@@ -28,19 +29,35 @@ def get_image_path(directory, name):
 
     Returns:
         path to the image
+
     """
-    return os.path.join(directory, f"{name}.jpg")
+    return Path(directory) / f"{name}.jpg"
 
 
 def save_labels(directory, labels):
-    with open(get_label_path(directory), "w") as f:
+    """Save labels to a JSON file in a given directory.
+
+    Args:
+        directory: path to where to save the labels.json file.
+        labels: dictionary with labels to be saved
+
+    """
+    with Path(get_label_path(directory)).open("w") as f:
         json.dump(labels, f, indent=0)
 
 
 def load_labels(directory):
-    with open(get_label_path(directory), "r") as f:
-        labels = json.load(f)
-    return labels
+    """Load labels from a given directory.
+
+    Args:
+        directory: directory of the dataset
+
+    Returns:
+        json file with the labels of a log file
+
+    """
+    with Path(get_label_path(directory)).open() as f:
+        return json.load(f)
 
 
 def load_image(directory, label, **kwargs):
@@ -49,54 +66,78 @@ def load_image(directory, label, **kwargs):
     Args:
         directory: directory of the dataset
         label: corresponding label of the image
+        **kwargs: image format
 
     Returns:
         the image
+
     """
-    with open(get_image_path(directory, label["name"]), "rb") as f:
-        image = u_image.load_bhuman_jpeg_image(f.read(), **kwargs)
-    return image
+    with Path(get_image_path(directory, label["name"])).open("rb") as f:
+        return u_image.load_bhuman_jpeg_image(f.read(), **kwargs)
 
 
 def load_image_direct(path, **kwargs):
-    with open(path, "rb") as f:
-        image = u_image.load_bhuman_jpeg_image(f.read(), **kwargs)
-    return image
+    """Return an image from a direct path.
 
-def get_masks(label, object_name, input_dims= (480, 640), output_dims = (15, 20)):
-    """Generate an offset mask that converts the image coordinates of the object
-    into offsets relative to given cell dimensions. 
-    And and objectsness mask that marks the cell where the center of the object is in.
-    
+    Args:
+        path: path to the image file
+        **kwargs: image format
+
+    Returns:
+        the image
+
+    """
+    with Path(path).open("rb") as f:
+        return u_image.load_bhuman_jpeg_image(f.read(), **kwargs)
+
+
+def get_masks(label, object_name, input_dims=(480, 640), output_dims=(15, 20)) -> tuple:
+    """Return label masks that are used to train the encoder.
+
+    Generate an offset mask that converts the image coordinates of the object into offsets relative
+    to given cell dimensions. And and objectsness mask that marks the cell where the center of
+    the object is in.
+
     Args:
         label: label of the image
+        object_name: name of the object to generate masks for
         input_dims: the full dimensions of the camera image.
         output_dims: the number of cells. Should be the same dimensions of encoder output
-        
+
     Returns:
         an array of shape [output_dims_x, output_dims_y, 2]. Where the offset for each cell is
         portrayed in x and y coordinates.
+
     """
-
-    if(object_name not in label):
-
-        # If there are no objects of interest in the image all the offset get an arbitrary value. 
+    if object_name not in label:
+        # If there are no objects of interest in the image all the offset get an arbitrary value.
         # In the loss function all offsets of cell without an object are ignored anyway.
         offsets = tf.cast(tf.fill((*output_dims, 2), -1), dtype=tf.float32)
 
         # All cell are marked as false, as there are no object in the whole image.
-        objectness_mask = tf.fill(output_dims, False)
+        objectness_mask = tf.fill(output_dims, value=False)
 
         return offsets, objectness_mask
 
-    coordinates = list(label[object_name].values())[:-1] #Only take x and y coordinates (ignore radius)
+    coordinates = list(label[object_name].values())[
+        :-1
+    ]  # Only take x and y coordinates (ignore radius)
 
     # Make sure that input_dims are divisible by output_dims
     cell_dims = np.array(input_dims) // np.array(output_dims)
     scale = np.array(output_dims) / np.array(input_dims)
 
-    # Generate the cell grid in the full image scale (values point to upper left corner of each cell)
-    cells = tf.cast(tf.stack(tf.meshgrid(range(input_dims[1])[::cell_dims[1]], range(input_dims[0])[::cell_dims[0]]), axis=-1), dtype=tf.float32)
+    # Generate the cell grid in the full image scale
+    # (values point to upper left corner of each cell)
+    cells = tf.cast(
+        tf.stack(
+            tf.meshgrid(
+                range(input_dims[1])[:: cell_dims[1]], range(input_dims[0])[:: cell_dims[0]]
+            ),
+            axis=-1,
+        ),
+        dtype=tf.float32,
+    )
 
     offsets = coordinates - cells
 
@@ -104,7 +145,6 @@ def get_masks(label, object_name, input_dims= (480, 640), output_dims = (15, 20)
     offsets_scaled = offsets * scale
 
     # Mark all cells with true, where the value is between 0 and 1 (object is in that cell)
-    objectness_mask = [[all(n>=0 and n<1 for n in x) for x in row ] for row in offsets_scaled]
+    objectness_mask = [[all(n >= 0 and n < 1 for n in x) for x in row] for row in offsets_scaled]
 
     return offsets_scaled, objectness_mask
-
