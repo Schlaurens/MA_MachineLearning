00cdbec4ad22a2db6849b391b94116e1b7425db4
Laurens Schiefelbein <laurensschiefelbein@gmail.com>
2025-05-29 01:18:43 +0200

    add function to display extracted patches on a given image

--- util/image.py
+++ util/image.py
@@ -2,6 +2,7 @@ from enum import Enum
 
 import matplotlib.pyplot as plt
 import numpy as np
+import tensorflow as tf
 import turbojpeg
 from matplotlib import patches
 from PIL import Image
@@ -132,5 +133,60 @@ def show_cell_on_image(directory, label, object_name=None, grid_dims=(15, 20)):
                 facecolor=(255 / 255, 123 / 255, 0 / 255, 140 / 255),
             )
             ax.add_patch(rect_loss_mask)
+
+    plt.show()
+
+
+def show_patches_on_image(image, label, results):
+    """Draw the given image with rectangles that indicate the position of the extracted patches. And the patches in separate plots
+
+    Args:
+        image: the image in YUYV format [480, 640, 2]
+        label: the label of the object
+        results: the results from the patch extractor. Contains for each detected object
+            a number of patch candidates,
+            masks that indicate for each patch whether the center could be projected to the plane,
+            and the normalized corner coordinates of each patch (y1, x1, y2, x2)
+
+    """
+    image_res = image.shape[0:-1]
+    num_candidates = results[label][0].shape[1]
+
+    # Image colorspace conversion
+    image_yuv = tf.reshape(tf.constant(image), (-1, image_res[0], int(image_res[1] / 2), 4))
+    image_converted = tf.reshape((image_yuv), (-1, image_res[0], image_res[1], 2))
+
+    # Draw image with patches on top of it
+    _, axes = plt.subplots()
+    axes.imshow(image_converted[0, ..., 0] / 255)
+
+    for i, box in enumerate(results[label][2]):
+        # Coordinates for each box are y1, x1, y2, x2
+        # Upscale the normalized coordinates
+        coords = (box[1] * (image_res[1] - 1), box[0] * (image_res[0] - 1))
+        width = (box[3] - box[1]) * (image_res[1] - 1)
+        height = (box[2] - box[0]) * (image_res[0] - 1)
+
+        rect = patches.Rectangle(
+            coords,
+            width,
+            height,
+            linewidth=1,
+            edgecolor="black",
+            facecolor=(255 / 255, 123 / 255, 0 / 255, 0 / 255),
+        )
+        # Each patch has a number to identify the ordering
+        axes.text(x=(coords[0] + 4.0), y=coords[1] + 17.0, s=i + 1)
+        axes.add_patch(rect)
+
+    plt.title("Image")
+
+    # Draw the patch candidates in separate plots
+    _, axes = plt.subplots(num_candidates)
+    axes[0].imshow(results[label][0][0, 0, ..., 0].numpy() / 255)
+    axes[1].imshow(results[label][0][0, 1, ..., 0].numpy() / 255)
+    axes[2].imshow(results[label][0][0, 2, ..., 0].numpy() / 255)
+    axes[3].imshow(results[label][0][0, 3, ..., 0].numpy() / 255)
+    axes[4].imshow(results[label][0][0, 4, ..., 0].numpy() / 255)
 
     plt.show()
