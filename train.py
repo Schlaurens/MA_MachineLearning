4b6fa8f5ff104909437e26ae9f7edc489deccc32
Laurens Schiefelbein <laurensschiefelbein@gmail.com>
2025-06-16 17:16:03 +0200

    split dataset into train and validation set

--- train.py
+++ train.py
@@ -98,24 +98,43 @@ def get_data_info(directory="data"):
 
 def main():
     data = get_data_info()
-    train_ds = get_dataset(data["file_names"])
+    dataset = get_dataset(data["file_names"])
 
     epochs = 200
+    validation_split = 0.3
     batch_size = 32
     num_samples = num_samples = data["num_samples"]
-
+    train_samples = round(num_samples * (1 - validation_split))
+    val_samples = round(num_samples * validation_split)
+    
     print("Number of samples: ", num_samples)
+    print("Train Size: ", train_samples)
+    print("Val Samples: ", val_samples)
 
-    train_ds = train_ds.shuffle(batch_size, seed=42)
+    dataset = dataset.shuffle(batch_size, seed=42)
+
+    train_ds = dataset.take(train_samples)
+    val_ds = dataset.skip(val_samples)
+
     train_ds = train_ds.batch(batch_size)
+    val_ds = val_ds.batch(batch_size)
 
     # To counteract "Local rendezvous warning"
     train_ds = train_ds.repeat(-1)
+    val_ds = val_ds.repeat(-1)
 
     # Upper camera dimensions. Width is halved because of YUYV format
     model = FullModel(480, 320)
     model.compile(optimizer=tf.keras.optimizers.Adam())
-    model.fit(x=train_ds, epochs=epochs, steps_per_epoch=num_samples // batch_size)
+    model.fit(
+        x=train_ds,
+        validation_data=val_ds,
+        epochs=epochs,
+        steps_per_epoch=train_samples // batch_size,
+        validation_steps=val_samples // batch_size,
+    )
+
+    return model
 
 
 if __name__ == "__main__":
