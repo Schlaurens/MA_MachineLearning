04b3501a842c8a608712bd582439b91b4e060cc7
Laurens Schiefelbein <laurensschiefelbein@gmail.com>
2025-09-15 11:12:22 +0200

    Proper implementation of the custom checkpoint callback

--- src/util/callbacks.py
+++ src/util/callbacks.py
@@ -2,10 +2,21 @@ import tensorflow as tf
 
 
 class CustomCheckpointCallback(tf.keras.callbacks.Callback):
-    def __init__(self, filepath, timestamp, **kwargs):
+    """A custom callback to save checkpoints. This callback calls the save function for this specific model."""
+
+    def __init__(self, filepath, overwrite=True, verbose=False, **kwargs):
+        """Constructor
+
+        Args:
+            filepath: The path where the checkpoints will be saved to
+            overwrite: Whether files with the same name should be overwritten. Defaults to True.
+            verbose: Print the saving progress. Defaults to False.
+        """
         super().__init__()
         self.filepath = filepath
-        self.timestamp = timestamp
+        self.overwrite = overwrite
+        self.verbose = verbose
 
     def on_epoch_end(self, epoch, logs=None):
-        self.model.save(self.filepath, self.timestamp)
+        filename = f"epoch_{epoch}"
+        self.model.save(self.filepath, filename, self.overwrite, self.verbose)
