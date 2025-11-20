e519a4a544d45f9eeb74e8b9c7b279b13313cb36
Laurens Schiefelbein <laurens@uni-bremen.de>
2025-11-20 10:07:26 +0100

    change keybinds

--- src/browse.py
+++ src/browse.py
@@ -172,11 +172,11 @@ class BrowseApplication:
             self.label_mode = LabelMode.INTERSECTION_T
         elif event.key == "-":
             self.label_mode = LabelMode.INTERSECTION_X
-        elif event.key == "n":  # Unignore the (non-existing) intersection labels in this sample
+        elif event.key == "ö":  # Unignore the (non-existing) intersection labels in this sample
             current = int(self.slider_image.val)
             u_labels.set_ignore_intersection_sample_flag(self.labels[current], False)
             self.redraw_labels(self.labels[current])
-        elif event.key == "m":  # Ignore the (non-existing) intersection labels in this sample
+        elif event.key == "ä":  # Ignore the (non-existing) intersection labels in this sample
             current = int(self.slider_image.val)
             u_labels.set_ignore_intersection_sample_flag(self.labels[current], True)
             self.redraw_labels(self.labels[current])
