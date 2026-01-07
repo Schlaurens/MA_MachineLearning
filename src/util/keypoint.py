17e78a5a843200a7c78276b81f9d8dfcb0f0341d
Laurens Schiefelbein <laurensschiefelbein@gmail.com>
2026-01-07 22:09:01 +0100

    add a padding option

--- src/util/keypoint.py
+++ src/util/keypoint.py
@@ -39,24 +39,31 @@ def get_coords_from_offsets(offset_mask, image_dims=(480, 640)) -> tuple:
     return coords_masked
 
 
-def are_coords_in_patch(coords: tf.Tensor, boxes: tf.Tensor) -> tf.Tensor:
-    """_summary_
+def are_coords_in_patch(coords: tf.Tensor, boxes: tf.Tensor, padding: float = 0.2) -> tf.Tensor:
+    """Checks if a set of normalized coordinates are inside a patch.
 
     Args:
-        coords: the normalized coordinates to check [B, ..., 2]
-        boxes: the corner points of the patch (y1, x1, y2, x2) [B, ..., 4]
+        coords: The normalized coordinates to check [B, ..., 2]
+        boxes: The corner points of the patch (y1, x1, y2, x2) [B, ..., 4]
+        padding: The amount of padding as a fraction of the width of the box. Coordinates at the edge of the box inside the padding do not count as "inside the box". Defaults to 0.2
 
     Returns:
         A binary tensor where every row is True when the corresponding coords are inside the box. Else False. [B, ..., ]
     """
 
+    width = tf.math.subtract(boxes[..., 3], boxes[..., 1])
+    padding = width * padding
+
     # Check if the x coordinates are in the range of the boxes
     x_check = tf.logical_and(
-        tf.greater(coords[..., 0], boxes[..., 1]), tf.less(coords[..., 0], boxes[..., 3])
+        tf.greater(coords[..., 0], boxes[..., 1] + padding),
+        tf.less(coords[..., 0], boxes[..., 3] - padding),
     )  # [B, ..., ]
+
     # Check if the y coordinates are in the range of the boxes
     y_check = tf.logical_and(
-        tf.greater(coords[..., 1], boxes[..., 0]), tf.less(coords[..., 1], boxes[..., 2])
+        tf.greater(coords[..., 1], boxes[..., 0] + padding),
+        tf.less(coords[..., 1], boxes[..., 2] - padding),
     )  # [B, ..., ]
 
     return tf.where(
