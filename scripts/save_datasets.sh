c12437fbb404c61ce78df77e941dbd6e79b0a726
Laurens Schiefelbein <laurensschiefelbein@gmail.com>
2026-03-09 18:25:23 +0100

    adjust save_dataset bash script for varying image resolutions

--- scripts/save_datasets.sh
+++ scripts/save_datasets.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-
 # Suppress TensorFlow warnings and messages
 export TF_CPP_MIN_LOG_LEVEL=3  # Suppress TensorFlow logs (0 = all, 1 = info, 2 = warnings, 3 = errors)
 
 # Check if the parent directory is provided
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <parent_directory>"
+if [ "$#" -ne 4 ]; then
+    echo "Usage: $0 <parent_directory> <dest_directory> <height> <width>"
     exit 1
 fi
 
 parent_dir="$1"
+dest_dir="$2"
+height="$3"
+width="$4"
 
 # Check if the parent directory exists
 if [ ! -d "$parent_dir" ]; then
@@ -22,7 +24,7 @@ for dir in "$parent_dir"/*/; do
     if [ -d "$dir" ]; then
         echo "Generating .tfrecords file for directory: $dir"
         start_time=$(date +%s)
-        uv run src/dataset/save_dataset.py "$dir"
+        uv run src/dataset/save_dataset.py --src_dir "$dir" --dest_dir "$dest_dir" --image_res $height $width
         end_time=$(date +%s)
         elapsed=$((end_time - start_time))
         echo "Time taken: $elapsed seconds"
