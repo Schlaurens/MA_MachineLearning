82a525a5ec1777e9f59d399e7fec4114e04ac6ad
Laurens Schiefelbein <laurensschiefelbein@gmail.com>
2026-03-31 19:07:46 +0200

    add all relevant setting files to script

--- scripts/multi_training.sh
+++ scripts/multi_training.sh
@@ -2,11 +2,35 @@
 
 export TF_CPP_MIN_LOG_LEVEL=2
 
+# SETTINGSFILES=(
+#     "all_categories_240x320_v1.yaml"
+#     "all_categories_240x320_v2.yaml"
+#     "all_categories_240x320_v3.yaml"
+# )
+
+# SETTINGSFILES=(
+#     "all_categories_288x384_v1.yaml"
+#     "all_categories_288x384_v2.yaml"
+#     "all_categories_288x384_v3.yaml"
+# )
+
+# SETTINGSFILES=(
+#     "all_categories_360x480_v1.yaml"
+#     "all_categories_360x480_v2.yaml"
+#     "all_categories_360x480_v3.yaml"
+# )
+
 SETTINGSFILES=(
-    "all_categories_v1_240x320.yaml"
-    "all_categories_v2_240x320.yaml"
-    "all_categories_v3_240x320.yaml"
+    "all_categories_432x576_v1.yaml"
+    "all_categories_432x576_v2.yaml"
+    "all_categories_432x576_v3.yaml"
 )
+
+# SETTINGSFILES=(
+#     "all_categories_480x640_v1.yaml"
+#     "all_categories_480x640_v2.yaml"
+#     "all_categories_480x640_v3.yaml"
+# )
 
 for F in "${SETTINGSFILES[@]}"; do
     echo "Running with settings file: $F"
