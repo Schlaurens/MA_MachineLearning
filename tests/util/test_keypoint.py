0f853b1b62c17b7c78002efc10d4a1a799f145b6
Laurens Schiefelbein <laurensschiefelbein@gmail.com>
2026-05-15 17:24:40 +0200

    implement work in progress function that calculates the patch coordinates into image coordinates. There is still a bug in there

--- tests/util/test_keypoint.py
+++ tests/util/test_keypoint.py
@@ -1,6 +1,8 @@
+import pytest
 import tensorflow as tf
 
 from src.util import keypoint as u_keypoint
+from src.util import layers as u_layers
 
 
 class TestAreCoordsInPatch:
@@ -86,8 +88,8 @@ class TestAreCoordsInPatch:
         expected = tf.constant([[True, False, True], [True, False, True]])  # (2, 3)
         result = u_keypoint.are_coords_in_patch(
             coords[:, tf.newaxis, :],  # (2, 1, 2)
-            boxes,                      # (2, 3, 4)
-            padding=0
+            boxes,  # (2, 3, 4)
+            padding=0,
         )
         assert tf.reduce_all(result == expected)
 
@@ -100,3 +102,95 @@ class TestAreCoordsInPatch:
         result = u_keypoint.are_coords_in_patch(coordinates_normalized, box, padding=0.20)
 
         assert tf.reduce_all(result == expected)
+
+
+class TestPatchCoordsToImageCoords:
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        self.patch_dims = tf.constant([32, 32], tf.float32)
+        self.patch_center = tf.constant([15.5, 15.5], tf.float32)
+        self.extractor = u_layers.PatchExtractor()
+        self.camera = tf.constant(
+            [[-0.0220113918, 0.0786367953, 0.493571222]],
+            tf.float32,
+        )  # (B, 2, 3)
+        self.camera_intr = tf.constant([[320, 240, 618.663391, 617.159]], tf.float32)  # (B, 2, 4)
+
+    def test_base_case(self):
+        coords = tf.constant([[[30, 380], [400, 400]]], tf.float32)  # (B, 2, 2)
+
+        (_, _, _, pixel_sizes) = self.extractor(
+            tf.zeros((1, 480, 640, 1)), coords, self.camera, self.camera_intr
+        )
+
+        patch_coords = tf.constant([[15.5, 15.5], [15.5, 15.5]])
+
+        results = u_keypoint.patch_coords_to_image_coords(
+            self.patch_dims, pixel_sizes, patch_coords, coords
+        )
+
+        tf.print(results)
+
+        assert tf.reduce_all(coords == results)
+
+    def _run(self, patch_coords, image_coords, pixel_sizes):
+        return u_keypoint.patch_coords_to_image_coords(
+            self.patch_dims,
+            pixel_sizes,
+            patch_coords,
+            image_coords,
+        )
+
+    def test_patch_center_returns_image_coords(self):
+        """Patch center should map exactly to the image center coords."""
+        image_coords = tf.constant([[[100.0, 200.0]]])  # (1, 1, 2)
+        patch_coords = tf.constant([[[15.5, 15.5]]])  # (1, 1, 2)
+        pixel_sizes = tf.constant([[2.0]])  # (1, 1)
+
+        result = self._run(patch_coords, image_coords, pixel_sizes)
+        tf.debugging.assert_near(result, image_coords)
+
+    def test_top_left_corner(self):
+        """Patch (0,0) should be offset by -patch_center * pixel_size."""
+        image_coords = tf.constant([[[100.0, 200.0]]])
+        patch_coords = tf.constant([[[0.0, 0.0]]])
+        pixel_sizes = tf.constant([[2.0]])
+
+        expected = image_coords + (tf.constant([[[0.0, 0.0]]]) - self.patch_center) * 2.0
+        result = self._run(patch_coords, image_coords, pixel_sizes)
+        tf.debugging.assert_near(result, expected)
+
+    def test_bottom_right_corner(self):
+        """Patch (31,31) should be offset by +patch_center * pixel_size."""
+        image_coords = tf.constant([[[100.0, 200.0]]])
+        patch_coords = tf.constant([[[31.0, 31.0]]])
+        pixel_sizes = tf.constant([[2.0]])
+
+        expected = image_coords + (tf.constant([[[31.0, 31.0]]]) - self.patch_center) * 2.0
+        result = self._run(patch_coords, image_coords, pixel_sizes)
+        tf.debugging.assert_near(result, expected)
+
+    def test_top_left_and_bottom_right_are_symmetric(self):
+        """Top-left and bottom-right offsets should be equal and opposite."""
+        image_coords = tf.constant([[[100.0, 200.0], [100.0, 200.0]]])  # (1, 2, 2)
+        patch_coords = tf.constant([[[0.0, 0.0], [31.0, 31.0]]])
+        pixel_sizes = tf.constant([[2.0, 2.0]])
+
+        result = self._run(patch_coords, image_coords, pixel_sizes)
+        offset_tl = result[:, 0, :] - image_coords[:, 0, :]
+        offset_br = result[:, 1, :] - image_coords[:, 1, :]
+        tf.debugging.assert_near(offset_tl, -offset_br)
+
+    def test_pixel_size_scales_offset(self):
+        """Doubling pixel_size should double the offset from center."""
+        image_coords = tf.constant([[[100.0, 200.0], [100.0, 200.0]]])  # (1, 2, 2)
+        patch_coords = tf.constant([[[0.0, 0.0], [0.0, 0.0]]])
+        pixel_sizes_1x = tf.constant([[1.0, 1.0]])
+        pixel_sizes_2x = tf.constant([[2.0, 2.0]])
+
+        result_1x = self._run(patch_coords, image_coords, pixel_sizes_1x)
+        result_2x = self._run(patch_coords, image_coords, pixel_sizes_2x)
+
+        offset_1x = result_1x - image_coords
+        offset_2x = result_2x - image_coords
+        tf.debugging.assert_near(offset_2x, 2.0 * offset_1x)
